13535d92caa3f6addb3d065857952ff2fdededf4
yeasir ahamed <yahamed007@gmail.com>
2021-05-24 20:26:48 +0600

    Add useAuth hook

---
+++ client/src/utils/useAuth.js
@@ -0,0 +1,12 @@
+import {useContext} from 'react'
+import authContext from './context/auth/authContext'
+
+const useAuth = () => {
+  const context = useContext(authContext)
+  if (!context) {
+    throw new Error('Component must be wrapped with in AuthContextProvider')
+  }
+  return context
+}
+
+export default useAuth
