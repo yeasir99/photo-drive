49d58dfd88e429313a612b52058849e41d1c12af
yeasir ahamed <yahamed007@gmail.com>
2021-05-26 21:42:20 +0600

    Add unauthorized app

---
+++ client/src/screens/Unauthorizedapp.js
@@ -0,0 +1,35 @@
+import {Tabs, TabList, Tab, TabPanels, TabPanel} from '@reach/tabs'
+import '@reach/tabs/styles.css'
+import About from './../components/About'
+import Login from './../components/Login'
+import SignUp from './../components/SignUp'
+
+const Unauthorizedapp = () => {
+  return (
+    <div className="flex flex-col items-center h-screen ">
+      <About />
+      <div className="max-w-xs ">
+        <Tabs className="bg-blue-200 rounded-lg">
+          <TabList className="flex text-lg rounded-t-lg">
+            <Tab className="w-full outline-none focus:outline-none focus:bg-blue-200 rounded-tl-lg">
+              Login
+            </Tab>
+            <Tab className="w-full outline-none focus:outline-none focus:bg-blue-200 rounded-tr-lg">
+              Sign Up
+            </Tab>
+          </TabList>
+          <TabPanels className="px-4">
+            <TabPanel className="w-full">
+              <Login />
+            </TabPanel>
+            <TabPanel className="w-full">
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
+    </div>
+  )
+}
+
+export default Unauthorizedapp
