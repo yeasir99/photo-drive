e8285e98db938f022af465fa730eb71f1f503a94
Yeasir Ahamed <yahamed007@gmail.com>
2021-11-15 23:39:48 +0600

    add modal for Image

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,6 +1,14 @@
 import Spinner from './Spinner'
+import {useState} from 'react'
+import ModalAbst from './ModalAbst'
 
 function Gallery({loading, data}) {
+  const [open, setOpen] = useState({
+    image: '',
+    isOpen: false,
+  })
+  console.log(open)
+
   if (loading) {
     return (
       <div className="flex justify-center ">
@@ -10,12 +18,19 @@ function Gallery({loading, data}) {
   }
 
   return data.length > 0 ? (
-    <div className="grid grid-cols-5 gap-2 mt-2">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 mt-2">
       {data.map(item => (
-        <div key={item._id} className="cursor-pointer">
+        <div
+          key={item._id}
+          className="cursor-pointer"
+          onClick={() => {
+            setOpen({image: item, isOpen: true})
+          }}
+        >
           <img src={item.url} alt="gallery" className="w-full h-auto" />
         </div>
       ))}
+      <ModalAbst open={open} setOpen={setOpen} />
     </div>
   ) : (
     <p>No image to display first upload image</p>
---
+++ client/src/components/ModalAbst.js
@@ -0,0 +1,56 @@
+import Modal from 'react-modal'
+import {ImCross} from 'react-icons/im'
+import {FaCopy} from 'react-icons/fa'
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+}
+
+Modal.setAppElement('#root')
+
+function ModalAbst({open, setOpen, style}) {
+  const closeModal = () =>
+    setOpen({
+      isOpen: false,
+      image: '',
+    })
+
+  return (
+    <Modal
+      isOpen={open.isOpen}
+      onRequestClose={closeModal}
+      style={{...customStyles, ...style}}
+    >
+      <div>
+        <div>
+          <button
+            onClick={closeModal}
+            className=" border-2 border-red-700 p-2 rounded-full absolute right-0.5 top-0.5 z-10"
+          >
+            <ImCross className="text-red-500" />
+          </button>
+        </div>
+        <img src={open.image?.url} alt="gallery" className="w-full h-auto" />
+        <div>
+          <button
+            onClick={() => navigator.clipboard.writeText(open.image.url)}
+            className="mt-1 border-1 bg-green-400 rounded-md py-1 px-3"
+          >
+            <div className="flex justify-items-center items-center text-white">
+              <FaCopy className="mr-1" /> url
+            </div>
+          </button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export default ModalAbst
